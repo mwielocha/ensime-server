419e2d40d63ad6e46f5c670676e94c487fbb6c95
mwielocha <mwielocha@icloud.com>
2016-06-28 03:09:34 +0200

    fixed tests after moving to FullyQualifiedName in JavaAnalyzer

--- core/src/main/scala/org/ensime/core/javac/Helpers.scala
+++ core/src/main/scala/org/ensime/core/javac/Helpers.scala
@@ -5,15 +5,19 @@ package org.ensime.core.javac
 import akka.event.slf4j.SLF4JLogging
 import com.sun.source.tree._
 import com.sun.source.util.TreePath
-import javax.lang.model.`type`.{ DeclaredType, PrimitiveType, TypeKind, TypeMirror }
+import javax.lang.model.`type`._
 import javax.lang.model.element._
+import javax.lang.model.element.ElementKind._
 import scala.collection.JavaConversions._
 
-import org.ensime.api.deprecating
 import org.ensime.core.{ DocFqn, DocSig }
 import org.ensime.indexer._
 
 trait Helpers extends UnsafeHelpers with SLF4JLogging {
+
+  private implicit class EnhachedElement(e: Element) {
+    def isOf(kinds: ElementKind*): Boolean = kinds.exists(_ == e.getKind)
+  }
 
   def typeMirror(c: Compilation, t: Tree): Option[TypeMirror] = {
     Option(c.trees.getTypeMirror(c.trees.getPath(c.compilationUnit, t)))
@@ -30,14 +34,49 @@ trait Helpers extends UnsafeHelpers with SLF4JLogging {
         .flatMap(t => Option(c.types.asElement(t))))
   }
 
+  def toSymbolName(fqn: FullyQualifiedName): String = fqn match {
+    case m: MethodName =>
+
+      val owner = m.owner.fqnString
+      val name = m.name
+
+      s"$owner.$name"
+
+    case x => x.fqnString
+  }
+
   def fqn(c: Compilation, el: Element): Option[FullyQualifiedName] = el match {
     case e: ExecutableElement =>
 
-      descriptor(c, e).map {
+      descriptor(c, e).map { descriptor =>
+
+        val name = e.getSimpleName.toString
+        val params = descriptor.params
+          .map(_.fqnString).mkString(",")
+
         MethodName(
           ClassName.fromFqn(e.getEnclosingElement.toString),
-          e.getSimpleName.toString, _
+          s"$name($params)", descriptor
         )
+      }
+
+    case e: VariableElement if e.isOf(PARAMETER, LOCAL_VARIABLE) =>
+
+      Some(ClassName(PackageName(Nil), e.toString))
+
+    case e: VariableElement if e.isOf(FIELD) =>
+
+      Some(FieldName(
+        ClassName.fromFqn(
+          e.getEnclosingElement.toString
+        ),
+        e.getSimpleName.toString
+      ))
+
+    case e: VariableElement if e.isOf(ENUM_CONSTANT) =>
+
+      fqn(c, e.asType()).map {
+        FieldName(_, e.getSimpleName.toString)
       }
 
     case e => fqn(c, e.asType())
@@ -69,24 +108,25 @@ trait Helpers extends UnsafeHelpers with SLF4JLogging {
     // hierarchy since an implementation may choose to have a single
     // object implement multiple TypeMirror subinterfaces." --
     // TypeMirror docs
-    tm match {
-      case tm: DeclaredType if tm.getKind == TypeKind.DECLARED => {
-        tm.asElement match {
-          case te: TypeElement => Some(ClassName.fromFqn(te.getQualifiedName.toString))
-          case _ => {
-            None
-          }
-        }
-      }
-      case tm: PrimitiveType if tm.getKind.isPrimitive => Some(ClassName(PackageName(Nil), tm.toString))
-      case _ => None
-    }
+    // tm match {
+    //   case tm: DeclaredType if tm.getKind == TypeKind.DECLARED => {
+    //     tm.asElement match {
+    //       case te: TypeElement => Some(ClassName.fromFqn(te.getQualifiedName.toString))
+    //       case _ => {
+    //         None
+    //       }
+    //     }
+    //   }
+    //   case tm: PrimitiveType if tm.getKind.isPrimitive => Some(ClassName(PackageName(Nil), tm.toString))
+    //   case _ => None
+    // }
+    Some(ClassName.fromFqn(tm.toString))
   }
 
   def toDocSign(fqn: FullyQualifiedName): DocSig = fqn match {
     case p: PackageName => DocSig(DocFqn(p.parent.fqnString, p.path.last), None)
     case c: ClassName => DocSig(DocFqn(c.pack.fqnString, c.name), None)
-    case m: MethodName => DocSig(DocFqn(m.owner.fqnString, m.name), Some(m.name))
+    case m: MethodName => DocSig(DocFqn(m.owner.pack.fqnString, m.owner.name), Some(m.name))
     case f: FieldName => DocSig(DocFqn(f.owner.fqnString, f.name), Some(f.name))
   }
 }
